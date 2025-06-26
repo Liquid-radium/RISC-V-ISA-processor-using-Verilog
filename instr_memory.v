fd30ee4ae0cd9dcada250c62365b0ecad44a666c
liquid-radium <shruti.rhegde04@gmail.com>
2025-06-26 17:10:20 +0530

    single cycle architecture

--- instr_memory.v
+++ instr_memory.v
@@ -1,11 +1,8 @@
 module instr_memory(
     input [31:0] a,
     input rst,
-    output [31:0] rd
+    output wire [31:0] rd
 );
-
 reg [31:0] mem [0:1023];
-
-assign rd = (rst == 1'b0) ? 32'h00000000 : mem[a[11:2]];
-
+assign rd = (rst == 1'b0) ? 32'h00000000 : mem [a[11:2] ]; 
 endmodule
